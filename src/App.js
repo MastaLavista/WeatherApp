0fa6b3c7c1d52924a3c1293f0ccf4610ca89a868
RublevD <danil.r@devhouse.pro>
2020-04-23 21:14:36 +0300

    Fix some troubles
    Change appid key. Change request params (replace search by city to zip). Add some request logs. Pass missed name property to WeatherDisplay

--- src/App.js
+++ src/App.js
@@ -1,71 +1,69 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 const PLACES = [
-  { name: "Palo Alto", zip: "94303"},
-  { name: "San Jose", zip: "94088"},
-  { name: "Santa Cruz", zip: "95062"},
-  { name: "Honolulu", zip: "96803"}
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" },
 ];
 
 class WeatherDisplay extends Component {
   constructor() {
-      super();
-      this.state = {
-        weatherData: null
-      };
+    super();
+    this.state = {
+      weatherData: null,
+    };
   }
 
   componentDidMount() {
-    const name = this.props.name;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" + 
-    name + 
-    "&appid=1ce8ad64298823fad398d6e779e270c";
-    fetch(URL).then(res => res.json()).then (json => {
-      this.setState({weatherData: json});
-    });
+    const { zip, name } = this.props;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=6d532ee533971e2453b7a114726bcc99`;
+    fetch(URL)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({ weatherData: json });
+      });
   }
   render() {
-    const {weatherData} = this.state;
+    const { weatherData } = this.state;
     if (!weatherData) return <div>Loading</div>;
     const weather = weatherData.weather;
-    console.log('weather', weather, this.props);
+    console.log("weather", weather);
     const iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
-    return (
-      <h1>Посмотрим на погоду for в городе {this.props.name}</h1>
-    );
-    }
+    return <h1>Посмотрим на погоду for в городе {this.props.name}</h1>;
+  }
 }
 
 //not at all
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       activePlace: 0,
     };
   }
-  render () {
+  render() {
     const activePlace = this.state.activePlace;
-   return (
-    <div className="App">
-      {PLACES.map((place, index) => (
-        <button
-        key = {index}
-        onClick = {() => {
-         this.setState({ activePlace: index});
-        }}
-      >
-        {place.name}
-      </button>
-    ))}
-     <WeatherDisplay 
-      key = {activePlace}
-      zip = {PLACES[activePlace].zip}
-      />
-    </div>
-  );
- }
+    return (
+      <div className="App">
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} name={PLACES[activePlace].name} />
+      </div>
+    );
+  }
 }
 
 export default App;
